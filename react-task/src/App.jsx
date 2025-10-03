a457ac7b647e29c48af282ea65dcc3d2793875bd
iamtayyab007 <tayyabm336@gmail.com>
2025-10-03 11:38:37 +0500

    updated the folder with coding and new components

--- react-task/src/App.jsx
+++ react-task/src/App.jsx
@@ -3,12 +3,14 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import PractiseTask from "./component/PractiseTask";
+import Practise from "./component/Practise";
 
 function App() {
   return (
     <>
       <div>
         <PractiseTask />
+        <Practise />
       </div>
     </>
   );
